f2253b502c56db4d053fc48fb7883f4a18ed50cb
Yashaswini Subramaniam <ysubrama@purdue.edu>
2025-05-01 15:59:15 -0400

    Initial commit

--- backend/index.js
+++ backend/index.js
@@ -21,13 +21,15 @@ sequelize.sync({ alter: true }).then(() => {
 });
 
 // MySQL Database Connection
-const db = mysql.createConnection({
-  host: "localhost",
-  user: "root", 
-  password: "Gyashaswini@123",
-  database: "job_tracker",
-});
+// filepath: c:\Users\yasha\Documents\GitHub\react-app1\backend\index.js
+dotenv.config();
 
+const db = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
 // Connect to MySQL
 db.connect((err) => {
   if (err) {

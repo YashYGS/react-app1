da542bc33749cdc1debdb8dfd764547b2e663cca
Yashaswini Subramaniam <ysubrama@purdue.edu>
2025-05-01 21:00:06 -0400

    sequalize check

--- backend/db.js
+++ backend/db.js
@@ -5,4 +5,13 @@ const sequelize = new Sequelize("job_tracker", "root", "Gyashaswini@123", {
   dialect: "mysql",
 });
 
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log("✅ Connection to the database has been established successfully.");
+  })
+  .catch((err) => {
+    console.error("❌ Unable to connect to the database:", err);
+  });
+  
 export default sequelize;

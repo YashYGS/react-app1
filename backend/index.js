05fe2b4815f033cc6734c76a913c57caa014f2e6
Yashaswini Subramaniam <97842885+YashYGS@users.noreply.github.com>
2025-05-02 22:55:02 -0400

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -33,7 +33,7 @@ sequelize.sync({ alter: true }).then(() => {
 const db = mysql.createConnection({
   host: "34.9.87.219",
   user: "root", 
-  password: "Gyashaswini@123",
+  password: "Hello@111",
   database: "job_tracker",
 });
 
